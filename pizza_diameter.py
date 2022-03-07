b752785ed2810f894ece080d5e06f2fe50be94fd
michael-clermont1 <60712743+michael-clermont1@users.noreply.github.com>
2022-03-07 14:03:14 -0500

    init

---
+++ pizza_diameter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Created by: Michael Clermont
+# Created on: Feb 2022
+# This program calculates the price of a pizza
+#    with diameter inputted by the user and HST
+
+
+
+def main():
+    # this function calculates the circumference
+
+    # input
+
+    # process
+
+    # output
+    print("")
+    print(" {1:,.2f}".format())
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
